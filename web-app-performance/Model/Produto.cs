a1f01f38b78f2208b9c48f6245580bb5994920c6
JoaoVictorRossi <jrossi4002@gmail.com>
2024-09-28 16:38:35 -0300

    ProdutoController e Model

---
+++ web-app-performance/Model/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace web_app_performance.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal preco { get; set; }
+        public int quantidade_estoque { get; set; }
+        public DateTime data_criacao { get; set; }
+    }
+}
